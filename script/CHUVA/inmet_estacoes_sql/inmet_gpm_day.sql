d87096fc49696a9bb123ca5aad5dfe458fdcf416
lmotta <motta.luiz@gmail.com>
2021-10-03 11:16:15 -0300

    Delete inmet_gpm_day.sql

--- script/CHUVA/inmet_estacoes_sql/inmet_gpm_day.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-	code_wmo,
-	strftime('%Y', v_date) AS v_year, strftime('%m', v_date) AS v_month, strftime('%d', v_date) AS v_day, 
-	inmet_prep, gpm_prep
-FROM stations_cerrado_prep_inmet_gpm
-ORDER BY code_wmo, v_year, v_month, v_day
